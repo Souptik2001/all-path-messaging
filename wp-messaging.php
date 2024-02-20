879eb30c7eb1bd5ceb6a7f8b1c39d248a6bf7646
Souptik Datta <souptikdatta2001@gmail.com>
2024-02-20 21:28:39 +0530

    Bootstrap project
    Signed-off-by: Souptik Datta <souptikdatta2001@gmail.com>

---
+++ wp-messaging.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name: WP Messaging
+Description: Limitless Communication: All-in-One, super scalable, messaging Solution for WordPress.
+Version: 1.0.0
+Author: Souptik Datta
+Author URI: https://souptik.dev
+Text Domain: wp-messaging
+*/
+
+define( 'SD_WP_MESSAGING_PATH', __DIR__ );
+
+require_once SD_WP_MESSAGING_PATH . '/inc/namespace.php';
+require_once SD_WP_MESSAGING_PATH . '/inc/helpers.php';
