1e4871a6074b351afc9f2da7484b7cd50002b168
Souptik Datta <souptikdatta2001@gmail.com>
2024-10-12 20:58:24 +0530

    fix: Fix prepare-plugin script
    Signed-off-by: Souptik Datta <souptikdatta2001@gmail.com>

--- .bin/prepare.sh
+++ .bin/prepare.sh
@@ -8,12 +8,19 @@ git config --global user.name "GitHub Deploy"
 
 echo $BUILT_BRANCH
 
+# Make the backup of the parent branch.
+cp -r $GITHUB_WORKSPACE $HOME/parent-branch
+
+# Checkout to the built branch.
 git checkout $BUILT_BRANCH || git checkout -b $BUILT_BRANCH
 
-# Merge the parent branch
-git merge $PARENT_BRANCH
+# Remove everything from built branch.
+rm -rf *
 
-# Cleanup
+# Copy the main branch content to built branch.
+cp -r $HOME/parent-branch/* .
+
+# Cleanup.
 rm -f README.md
 rm -rf .tests
 rm -rf .github
@@ -30,9 +37,9 @@ rm -f phpcs.xml
 rm -f phpstan.neon
 rm -rf node_modules
 
-# Check if we have changes
+# Check if we have changes.
 if [[ -z $(git status -s) ]]; then
-	# No changes bail out
+	# No changes bail out.
 	echo "No changes to push"
 else
 	# Push the changes!
